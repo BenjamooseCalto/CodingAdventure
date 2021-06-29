9176f8522cb5bfcc6490bd1283b7d7d3d21410d6
Ben <Bcaltons5@gmail.com>
2021-06-29 02:32:45 -0500

    Update .gitignore

--- WoW/ref/Listener/locale/deDE.lua
+++
@@ -1,9 +0,0 @@
--- LISTENER by Tammya-MoonGuard (2018)
-
-if GetLocale() ~= "deDE" then return end
-
--------------------------------------------------------------------------------
-local Main = ListenerAddon
-local L    = Main.Locale
-
--------------------------------------------------------------------------------
